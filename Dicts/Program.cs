3320b6ca41bdf0a9f05397445820b308faf74f2e
Andrew Borges <andrewborges71@gmail.com>
2021-02-15 18:08:22 -0300

    exercicio sobre dictionaries

---
+++ Dicts/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Dicts
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> candidates = new Dictionary<string, int>();
+
+            Console.Write("Enter file full path: ");
+            string path = Console.ReadLine();
+
+            try
+            {
+                using StreamReader sr = File.OpenText(path);
+                while (!sr.EndOfStream)
+                {
+                    string[] votingRecord = sr.ReadLine().Split(',');
+                    string name = votingRecord[0];
+                    int votes = int.Parse(votingRecord[1]);
+
+                    if (candidates.ContainsKey(name))
+                    {
+                        candidates[name] += votes;
+                    }
+                    else
+                    {
+                        candidates[name] = votes;
+                    }
+                }
+            }
+            catch(IOException e)
+            {
+                Console.WriteLine("An error occured");
+                Console.WriteLine(e.Message);
+            }
+
+            foreach(KeyValuePair<string, int> candidate in candidates)
+            {
+                Console.WriteLine($"{candidate.Key}: {candidate.Value}");
+            }
+        }
+    }
+}
